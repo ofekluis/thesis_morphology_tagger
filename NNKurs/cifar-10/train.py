d9273fd19d810f371337ac31cbae407dee846d27
Ofek Luis Lewinsohn <o.l.lewinsohn@gmail.com>
2018-01-15 11:25:00 +0100

    my cifar-10 trains

--- NNKurs/cifar-10/train.py
+++ NNKurs/cifar-10/train.py
@@ -1,6 +1,3 @@
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-
 import tensorflow as tf
 import numpy as np
 
@@ -16,10 +13,16 @@ x_test = np.array([ex[0] for ex in test.data], dtype=np.float32)
 y_test = np.array([ex[1] for ex in test.data], dtype=np.int32)
 
 # del(train, test)  # frees approximately 180 MB
-print("x first dimension:", len(x_test))
-print("x second dimension:", len(x_test[0]))
-print(len(x_test[0][0]))
-print(len(x_test[0][0][0]))
+print("x first dimension:", len(x_train))
+print("x second dimension:", len(x_train[0]))
+print(len(x_train[0][0]))
+print(len(x_train[0][0][0]))
+print(len(y_train))
+print(y_train[0],y_train[1])
+# make one_hot 10 long array's from y
+y_train = [[1.0 if y_train[j]==i else 0.0 for i in range(10)] for j in range(len(y_train))]
+y_test = [[1.0 if y_test[j]==i else 0.0 for i in range(10)] for j in range(len(y_test))]
+print(y_train[0], y_train[1])
 
 # standardization
 x_train_pixel_mean = x_train.mean(axis=0)  # per-pixel mean
@@ -30,7 +33,7 @@ x_test -= x_train_pixel_mean
 x_test /= x_train_pixel_std
 
 
-x = tf.placeholder(tf.float32, shape=[None, 784])
+x = tf.placeholder(tf.float32, shape=[None, 32, 32,3])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
 def weight_variable(shape):
@@ -49,11 +52,11 @@ def max_pool_2x2(x):
 	return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
 		strides=[1, 2, 2, 1], padding='SAME')
 
-W_conv1 = weight_variable([5, 5, 1, 32])
+W_conv1 = weight_variable([5, 5, 3, 32])
 b_conv1 = bias_variable([32])
 
 # reshape input pictures to 4d so we can convolve over it with our filter
-x_image = tf.reshape(x, [-1, 28, 28, 1])
+x_image = tf.reshape(x, [-1, 32, 32, 3])
 
 # ReLU + max pooling
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
@@ -65,12 +68,12 @@ b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
-
+print(h_pool2)
 # reshape tensor into a batch of vectors, muliply by weights add bias and apply ReLU
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
+W_fc1 = weight_variable([8*8*64, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 8*8*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 # apply dropout to avoid overfitting
@@ -83,6 +86,7 @@ W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
 
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+print("y_conv:", y_conv)
 
 # train in batches, using adam optimizier (instead of steepest gradient decent).
 # log every 100th and use non interactive mode.
@@ -92,19 +96,19 @@ cross_entropy = tf.reduce_mean(
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
+bSize=50
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
-  for i in range(20000):
-    batch = mnist.train.next_batch(50)
-    if i % 100 == 0:
+  for i in range(0,50000-bSize,bSize):
+    batch = [x_train[i:i+bSize], y_train[i:i+bSize]]
+    if i % bSize == 0:
       train_accuracy = accuracy.eval(feed_dict={
           x: batch[0], y_: batch[1], keep_prob: 1.0})
-      print('step %d, training accuracy %g' % (i, train_accuracy))
+      print('step %d, training accuracy %g' % (i/50, train_accuracy))
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
   print('test accuracy %g' % accuracy.eval(feed_dict={
-      x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-
+      x: x_test, y_: y_test, keep_prob: 1.0}))
 exit("finished")
+
 
