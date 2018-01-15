1f0cf730ee0aee335bad6dad3cf51ed1d434d1f8
Ofek Luis Lewinsohn <o.l.lewinsohn@gmail.com>
2018-01-15 14:50:16 +0100

    architecture from hw first try

--- NNKurs/cifar-10/train.py
+++ NNKurs/cifar-10/train.py
@@ -13,16 +13,9 @@ x_test = np.array([ex[0] for ex in test.data], dtype=np.float32)
 y_test = np.array([ex[1] for ex in test.data], dtype=np.int32)
 
 # del(train, test)  # frees approximately 180 MB
-print("x first dimension:", len(x_train))
-print("x second dimension:", len(x_train[0]))
-print(len(x_train[0][0]))
-print(len(x_train[0][0][0]))
-print(len(y_train))
-print(y_train[0],y_train[1])
 # make one_hot 10 long array's from y
 y_train = [[1.0 if y_train[j]==i else 0.0 for i in range(10)] for j in range(len(y_train))]
 y_test = [[1.0 if y_test[j]==i else 0.0 for i in range(10)] for j in range(len(y_test))]
-print(y_train[0], y_train[1])
 
 # standardization
 x_train_pixel_mean = x_train.mean(axis=0)  # per-pixel mean
@@ -52,8 +45,12 @@ def max_pool_2x2(x):
 	return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
 		strides=[1, 2, 2, 1], padding='SAME')
 
-W_conv1 = weight_variable([5, 5, 3, 32])
-b_conv1 = bias_variable([32])
+def global_average_pool(x):
+    return tf.reduce_mean(x,[1,2])
+    #return tf.nn.avg_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+W_conv1 = weight_variable([5, 5, 3, 10])
+b_conv1 = bias_variable([10])
 
 # reshape input pictures to 4d so we can convolve over it with our filter
 x_image = tf.reshape(x, [-1, 32, 32, 3])
@@ -62,31 +59,39 @@ x_image = tf.reshape(x, [-1, 32, 32, 3])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1) # after this the image is 14x14 big.
 
-# second layer 64 dimension for the output feature map
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+# second layer 20 dimension for the output feature map
+W_conv2 = weight_variable([4, 4, 10, 20])
+b_conv2 = bias_variable([20])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
-print(h_pool2)
+
+# 3rd conv layer
+W_conv3 = weight_variable([3, 3, 20, 30])
+b_conv3 = bias_variable([30])
+
+h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
+h_pool3 = global_average_pool(h_conv3)
 # reshape tensor into a batch of vectors, muliply by weights add bias and apply ReLU
-W_fc1 = weight_variable([8*8*64, 1024])
-b_fc1 = bias_variable([1024])
+W_fc1 = weight_variable([30, 10])
+b_fc1 = bias_variable([10])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 8*8*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-# apply dropout to avoid overfitting
 keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+h_fc1_drop = tf.nn.dropout(h_pool3, keep_prob)
+
+y_conv = tf.matmul(h_fc1_drop, W_fc1) + b_fc1
+
+
+#print(y_conv)
+#exit("finished")
+# apply dropout to avoid overfitting
 
 # readout layer, do softmax to get some output y, remember we have 10 number
 # therefore we use 10 output dimension, for the classification.
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
+#W_fc2 = weight_variable([1024, 10])
+#b_fc2 = bias_variable([10])
 
-y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-print("y_conv:", y_conv)
+#y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 # train in batches, using adam optimizier (instead of steepest gradient decent).
 # log every 100th and use non interactive mode.
@@ -109,6 +114,5 @@ with tf.Session() as sess:
 
   print('test accuracy %g' % accuracy.eval(feed_dict={
       x: x_test, y_: y_test, keep_prob: 1.0}))
-exit("finished")
 
 
