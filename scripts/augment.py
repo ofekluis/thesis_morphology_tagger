228b4bd1b080c978e257f1694128156b0d03c1ae
Ofek Luis Lewinsohn <o.l.lewinsohn@gmail.com>
2018-03-24 14:00:56 +0100

    augment has flags now and can also not augment and just join 2 files

--- scripts/augment.py
+++ scripts/augment.py
@@ -1,26 +1,34 @@
-"""adds augmented symbols after every token in every line of both files
-the results is a joined file with augmented tokens, lines alternate between languages"
-args: filename, token, output_filename"""
-from sys import argv
-def augment():
-    with open(argv[1], "r") as lang1:
-        with open(argv[2], "r") as lang2:
-            with open(argv[3], "w") as outF:
-                lineLst=[[],[]]
-                lineLst[0]=[line.strip().split() for line in lang1.readlines()]
-                lineLst[1]=[line.strip().split() for line in lang2.readlines()]
-                for i,(line1,line2) in enumerate(zip(lineLst[0],lineLst[1])):
-                    line1=" ".join("*"+token+"*" for token in line1)
-                    line2=" ".join("^"+token+"^" for token in line2)
-                    print(line1,file=outF)
-                    print(line2,file=outF)
+"""adds augmented symbols to each token in every line of both files
+the results is a joined file with augmented tokens, lines alternate between languages
+can also be used to just concatenate two files (with delibarte mixing of the lines)"""
+import argparse
+def augment(doc1, doc2, out, sym1, sym2, aug):
+    sym= (sym1,sym2) if aug else ("","")
+    doc=[doc1,doc2]
+    with open(doc[0], "r") as doc[0]:
+        with open(doc[1], "r") as doc[1]:
+            with open(out, "w") as outF:
+                lineLst=[[line.strip().split() for line in d.readlines()] for d in doc]
+                for i,lines in enumerate(zip(lineLst[0],lineLst[1])):
+                    for line,s in zip(lines,sym):
+                        line=" ".join(s+token+s for token in line)
+                        print(line, file=outF)
                 # add left lines from the longer file
                 rest=1 if i>len(lineLst[0]) else 0
-                sym=("*","^")[rest]
                 for j in range(i+1,len(lineLst[rest])):
-                    line2=" ".join(sym+token+sym for token in lineLst[rest][j])
-                    print(line2,file=outF)
+                    line=" ".join(sym[rest]+token+sym[rest] for token in lineLst[rest][j])
+                    print(line,file=outF)
 
 
 if __name__ == "__main__":
-    augment()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--doc1", help="Choose first document to join", default="x_trn_nl")
+    parser.add_argument("--doc2", help="Choose second document to join", default="x_trn_vl")
+    parser.add_argument("--sym1", help="Choose augmentation symbol for first document's tokens", default="*")
+    parser.add_argument("--sym2", help="Choose augmentation symbol for second document's tokens", default="^")
+    parser.add_argument("--out", help="Choose joined document path/name", default="./aug")
+    parser.add_argument("--aug", help="Should documents be augmented or only joined?", default=True)
+    args = parser.parse_args()
+    if args.aug=="false" or args.aug=="False":
+        args.aug=False
+    augment(args.doc1,args.doc2,args.out,args.sym1,args.sym2,args.aug)
